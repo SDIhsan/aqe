27d128c634a9abf8f5b3b3040cd60f85eb8877c4
SDIhsan <n.ihsan998@gmail.com>
2022-06-30 20:20:55 +0700

    s

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'postgres://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj',
 	'hostname' => 'ec2-3-224-8-189.compute-1.amazonaws.com',
 	'username' => 'ojphctvhuchwhu',
 	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
