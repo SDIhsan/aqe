924e70cbb3db08d39d41a96616a70f37a8c7d536
SDIhsan <n.ihsan998@gmail.com>
2022-07-01 10:44:09 +0700

    s

--- application/config/database.php
+++ application/config/database.php
@@ -81,30 +81,7 @@ $db['default'] =
 	'username' => 'ojphctvhuchwhu',
 	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
 	'database' => 'ddblbnn9smemgj',
-	'dbdriver' => 'Postgre',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-];
-
-$db['pdo'] = 
-[
-	'dsn' => 'pgsql:host=ec2-3-224-8-189.compute-1.amazonaws.com;port=5432;dbname=ddblbnn9smemgj',
-	'hostname' => 'ec2-3-224-8-189.compute-1.amazonaws.com',
-	'username' => 'ojphctvhuchwhu',
-	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
-	'database' => 'ddblbnn9smemgj',
-	'dbdriver' => 'pdo',
+	'dbdriver' => 'pgsql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
