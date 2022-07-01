fd798481ed78c6378a84c9ec361f42faa13faa39
SDIhsan <n.ihsan998@gmail.com>
2022-07-01 14:55:14 +0700

    s

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,12 +3,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    // public function __construct()
-    // {
-    //     parent::__construct();
-    //     $this->load->library('form_validation');
-    //     $this->load->model('Auth_model', 'auth');
-    // }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('Auth_model', 'auth');
+    }
 
     private function _has_login()
     {
