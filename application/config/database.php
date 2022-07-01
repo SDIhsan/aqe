71f018cb2ccd1d1874dc781d29f140d24cbf12ca
SDIhsan <n.ihsan998@gmail.com>
2022-07-01 10:11:05 +0700

    s

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = 
 [
-	'dsn'	=> 'postgres://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj',
+	'dsn'	=> 'postgres://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj?charset=utf8&connect_timeout=5&sslmode=1',
 	'hostname' => '',
 	'username' => '',
 	'password' => '',
@@ -99,7 +99,7 @@ $db['default'] =
 $default['failover'] = 
 [
 [
-	'dsn'	=> 'Postgre://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj',
+	'dsn'	=> 'Postgre://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj?charset=utf8&connect_timeout=5&sslmode=1',
 	'hostname' => '',
 	'username' => '',
 	'password' => '',
