19aebc00b8477d6f344e460af35041bda7309aab
SDIhsan <n.ihsan998@gmail.com>
2022-07-01 14:23:54 +0700

    s

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] =
 	'username' => 'ojphctvhuchwhu',
 	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
 	'database' => 'ddblbnn9smemgj',
-	'dbdriver' => 'Postgre',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -97,48 +97,48 @@ $db['default'] =
 	'save_queries' => TRUE
 ];
 
-$default['failover'] = 
-[
-[
-	'dsn'	=> 'Postgre://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj?charset=utf8&connect_timeout=5&sslmode=1',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => '',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-],
-[
-	'dsn'	=> '',
-	'hostname' => 'ec2-3-224-8-189.compute-1.amazonaws.com:5432',
-	'username' => 'ojphctvhuchwhu',
-	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
-	'database' => 'ddblbnn9smemgj',
-	'dbdriver' => 'postgres',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-]
-];
+// $default['failover'] = 
+// [
+// [
+// 	'dsn'	=> 'Postgre://ojphctvhuchwhu:02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e@ec2-3-224-8-189.compute-1.amazonaws.com:5432/ddblbnn9smemgj?charset=utf8&connect_timeout=5&sslmode=1',
+// 	'hostname' => '',
+// 	'username' => '',
+// 	'password' => '',
+// 	'database' => '',
+// 	'dbdriver' => '',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// ],
+// [
+// 	'dsn'	=> '',
+// 	'hostname' => 'ec2-3-224-8-189.compute-1.amazonaws.com:5432',
+// 	'username' => 'ojphctvhuchwhu',
+// 	'password' => '02ecf81f400a7553230043d12efe01cbabb9f15c0940136c74a9738cd45b617e',
+// 	'database' => 'ddblbnn9smemgj',
+// 	'dbdriver' => 'postgres',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// ]
+// ];
